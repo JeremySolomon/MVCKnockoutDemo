9c1b1212ecc4afc63f3ac97912a2c7dbf9200b85
Jeremy Solomon <jsolomon@hmbnet.com>
2018-03-22 13:24:50 -0400

    Now with knockout

--- MVCKnockoutDemo/Controllers/HomeController.cs
+++ MVCKnockoutDemo/Controllers/HomeController.cs
@@ -1,16 +1,51 @@
-﻿using System;
+﻿using MVCKnockoutDemo.ViewModels;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MVCKnockoutDemo.Controllers
 {
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
-            return View();
+            ViewBag.Countries = new List<Country>(){
+                new Country()
+                {
+                    Id = 1,
+                    Name = "India"
+                },
+                new Country()
+                {
+                    Id = 2,
+                    Name = "USA"
+                },
+                new Country()
+                {
+                    Id = 3,
+                    Name = "France"
+                }
+            };
+
+            var viewModel = new PersonViewModel()
+            {
+                Id = 1,
+                Name = "Naveen",
+                DateOfBirth = new DateTime(1990, 11, 21)
+            };
+
+            return View(viewModel);
         }
+
+        [HttpPost]
+        public JsonResult SavePersonDetails(PersonViewModel viewModel)
+        {
+            // TODO: Save logic goes here.
+
+            return Json(new { });
+        }
+
+
     }
 }
